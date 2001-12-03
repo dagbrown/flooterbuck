08c626f0ad510953c53d53164a3073dcf04ff9e9
dagbrown <dagbrown>
2001-12-03 23:59:25 +0000

    Code cleanup.

--- modules/forget.pm
+++ modules/forget.pm
@@ -1,39 +1,47 @@
+#------------------------------------------------------------------------
+# forget.pm
+#
+# A module to let people tell infobot to forget factoids.
+#------------------------------------------------------------------------
+
 use strict;
 package forget;
 
 sub scan(&$$)
 {
-  my ($callback,$message,$who) = @_;
+    my ($callback,$message,$who) = @_;
 
-  if ($message =~ s/^forget\s+((a|an|the)\s+)?//i)
-  {
-    # cut off final punctuation
-    $message =~ s/[.!?]+$//;
-    my $k = &::normquery($message);
-    $k = lc($k);
-    my $found = 0;
-    foreach my  $d ("is", "are")
-    {
-      if (my $r = &::get($d, $k))
-      {
-        if (&::IsFlag("r") ne "r")
-        { $callback->("you have no access to remove factoids"); return 'NOREPLY'; }
-        $found = 1 ;
-        &::status("forget: <$who> $k =$d=> $r");
-        &::clear($d, $k);
-        $::factoidCount--;
-      }
-    }
-    if ($found == 1)
-    {
-      $callback->("$who: I forgot $k"); 
-      my $l = $who; $l =~ s/^=//;
-      $::updateCount++;
-      return 'NOREPLY';
-    }
-    else
-    { $callback->($who, "I didn't have anything matching $k"); return 'NOREPLY'; }
-  }                           # end forget
-  return undef;
+    if ($message =~ s/^forget\s+((a|an|the)\s+)?//i) {
+        # cut off final punctuation
+        $message =~ s/[.!?]+$//;
+        my $k = &::normquery($message);
+        $k = lc($k);
+        my $found = 0;
+
+        foreach my  $d ("is", "are") {
+            if (my $r = &::get($d, $k)) {
+                if (&::IsFlag("r") ne "r") { 
+                    $callback->("you have no access to remove factoids"); 
+                    return 'NOREPLY'; 
+                }
+                $found = 1 ;
+                &::status("forget: <$who> $k =$d=> $r");
+                &::clear($d, $k);
+                $::factoidCount--;
+            }
+        }
+
+        if ($found == 1) {
+            $callback->("$who: I forgot $k"); 
+            my $l = $who; $l =~ s/^=//;
+            $::updateCount++;
+            return 'NOREPLY';
+        } else { 
+            $callback->($who, "I didn't have anything matching $k"); 
+            return 'NOREPLY'; 
+        }
+    }                           # end forget
+    return undef;
 }
+
 "forget";
