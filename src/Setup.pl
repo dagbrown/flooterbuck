33661de4e676684925b95e229880b51d4f01b545
dagbrown <dagbrown>
2001-12-04 01:06:07 +0000

    Cleaned up code (sorry for the big diff!) and removed some ST00P1D global variables.  (There are still quite a few ST00P1D globals, but less than there were before.)

--- src/Setup.pl
+++ src/Setup.pl
@@ -7,17 +7,17 @@ sub setup {
     &paramSetup();
 
     if ($param{VERBOSITY} > 1) {
-	my $params = "Parameters are:\n";
-	foreach (sort keys %param) {
-	    $params .= "   $_ -> $param{$_}\n";
-	}
-	&status($params);
+        my $params = "Parameters are:\n";
+        foreach (sort keys %param) {
+            $params .= "   $_ -> $param{$_}\n";
+        }
+        &status($params);
     }
 
     die "dbname is null" if (!$param{'dbname'});
 
     %dbs = ("is" => "$param{basedir}/$param{dbname}-is",
-	    "are" => "$param{basedir}/$param{dbname}-are");
+            "are" => "$param{basedir}/$param{dbname}-are");
     srand();
 
     $setup_time = scalar(localtime());
@@ -31,103 +31,85 @@ sub setup {
     $qCount = &get("is", "the qCount");
     $qEpochTime = &get("is", "the qEpochTime");
 
-    # things to say when people thank me
-    @welcomes = ('no problem', 'my pleasure', 'sure thing',
-		 'no worries', 'de nada', 'de rien', 'bitte', 'pas de quoi');
-
     # when i'm cofused and I have to reply
     @confused = ("huh?", 
-		 "what?", 
-		 "sorry...", 
-		 "i\'m not following you...",
-		 "excuse me?");
+            "what?", 
+            "sorry...", 
+            "i\'m not following you...",
+            "excuse me?");
 
     # when i recognize a query but can't answer it
     @dunno = ('i don\'t know', 
-	      'wish i knew',
-	      'i haven\'t a clue',
-	      'no idea',
-	      'bugger all, i dunno');
-
-
+            'wish i knew',
+            'i haven\'t a clue',
+            'no idea',
+            'bugger all, i dunno');
 
     # check the ignore parameter for a filename containing the
     # ignore list
 
     if ($param{ignore}) {
-	&openDBMx('ignore');
+        &openDBMx('ignore');
     }
-	
-    if ($param{sanePrefix}) {
-	for $d (qw/is are/) {
-	    my $dbname = $DBprefix.$d;
-	    my $sane = "$param{confdir}/$param{sanePrefix}";
-	    $sane .= "-$d.txt";
-	    if (-e $sane) {
-		&status("loading sane defines $sane");
-		&insertFile($dbname, $sane);
-	    } else {
-		&status("can't fine sane file $sane");
-	    }
-	}
 
-	if (! open IGNORE, "$param{'confdir'}/$param{sanePrefix}-ignore.txt") {
-	    &status("No fallback ignore file $param{'confdir'}/$param{sanePrefix}-ignore.txt");
-	} else {
-	    while (<IGNORE>) {
-		s/^\s+//;
-		s/\s+\#.*//;
-		chomp;
-		/\S/ && do {
-		    &postInc(ignore => $_);
-		    if ($param{'VERBOSITY'} > 0) {
-			&status("Adding $_ to ignore list (from sane).");
-		    }
-		};
-	    }
-	    close IGNORE;
-	}
+    if ($param{sanePrefix}) {
+        for $d (qw/is are/) {
+            my $dbname = $DBprefix.$d;
+            my $sane = "$param{confdir}/$param{sanePrefix}";
+            $sane .= "-$d.txt";
+            if (-e $sane) {
+                &status("loading sane defines $sane");
+                &insertFile($dbname, $sane);
+            } else {
+                &status("can't fine sane file $sane");
+            }
+        }
+
+        if (! open IGNORE, "$param{'confdir'}/$param{sanePrefix}-ignore.txt") {
+            &status("No fallback ignore file $param{'confdir'}/$param{sanePrefix}-ignore.txt");
+        } else {
+            while (<IGNORE>) {
+                s/^\s+//;
+                s/\s+\#.*//;
+                chomp;
+                /\S/ && do {
+                    &postInc(ignore => $_);
+                    if ($param{'VERBOSITY'} > 0) {
+                        &status("Adding $_ to ignore list (from sane).");
+                    }
+                };
+            }
+            close IGNORE;
+        }
     }
 
     if ($param{'plusplus'}) {
-	&openDBMx('plusplus');
+        &openDBMx('plusplus');
     }
 
     if ($param{'seen'}) {
-	&openDBMx('seen');
+        &openDBMx('seen');
     }
 
     # set up the users and ops
-	&status("Parsing User File");
+    &status("Parsing User File");
     &parseUserfile();
 
-	&status("Parsing Channel File");
-	# set up the channel file
-	&parseChannelfile();
-
-    # ways to say hello
-    @hello = ('hello', 
-	      'hi',
-	      'hey',
-	      'niihau',
-	      'bonjour',
-	      'hola',
-	      'salut',
-	      'que tal',
-	      'privet',
-	      "what's up");
+    &status("Parsing Channel File");
+    # set up the channel file
+    &parseChannelfile();
 
     $param{'maxKeySize'}  ||= 30; # maximum LHS length
-    $param{'maxDataSize'} ||= 200; # maximum total length
+        $param{'maxDataSize'} ||= 200; # maximum total length
 
-    if (!defined(@verb)) {
-	@verb = split(" ", "is are");
-	#  am was were does has can wants needs feels
-	#  handle s-v agreement for non-being verbs later
-    }
+        if (!defined(@verb)) {
+            @verb = split(" ", "is are");
+            #  am was were does has can wants needs feels
+            #  handle s-v agreement for non-being verbs later
+        }
 
     if (!defined(@qWord)) {
-	@qWord = split(" ", "what where who"); # why how when
+        @qWord = split(" ", "what where who"); # why how when
     }
 
     # do this ONCE per startup to amortize.  Still too much mem.
