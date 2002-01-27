77e7ba132eedce48f9ccd4d6d9f7ef141cfd9e43
rharman <rharman>
2002-01-27 18:37:11 +0000

    *** empty log message ***

--- modules/fuzzyclock.pm
+++ modules/fuzzyclock.pm
@@ -8,7 +8,7 @@
 # Make it psychic enough to figure out what timezone you're in, and have
 # it tell you the right time.  (Ha ha ha)
 #
-# $Id: fuzzyclock.pm,v 1.14 2002/01/26 19:32:20 rharman Exp $
+# $Id: fuzzyclock.pm,v 1.15 2002/01/27 18:37:11 rharman Exp $
 #------------------------------------------------------------------------
 
 use strict;
@@ -92,7 +92,7 @@ sub fuzzytime {
 sub scan(&$$) {
     my ($callback, $message, $who) = @_;
     
-    if($message =~ /^(?:what time (?:is it|do you have)|fuzzy?(?:clock|time)\??)$/i) {
+    if($message =~ /(?:what time (?:is it|do you have))|(?:fuzzy?(?:clock|time))\??/i) {
 	if(rand()>0.5) {
 		$callback->("It's ".fuzzytime.", $who.");
 	} else {
