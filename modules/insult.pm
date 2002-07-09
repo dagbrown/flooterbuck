f3f9a58c3c91c97e73f875a178ff0cb19920c87e
rharman <rharman>
2002-07-09 10:56:59 +0000

    quick-and-dirty non-forking "port" of the Extras.pl code of the insult feature.  'insult foo in german' still not supported.

---
+++ modules/insult.pm
@@ -0,0 +1,48 @@
+# Template infobot extension
+
+use strict;
+package insult;
+
+my $no_insult;
+BEGIN {
+	eval "use Net::Telnet ();";
+	$no_insult++ if ($@) ;
+}
+
+sub scan(&$$) {
+    my ($callback,$message,$who) = @_;
+
+    # Check $message, if it's what you want, then do stuff with it
+    if($message =~ /insult\ (\w+)?/)
+    {
+      my $nick = $1;
+      my $t = new Net::Telnet (Errmode => "return", Timeout => 3);
+      $t->Net::Telnet::open(Host => "insulthost.colorado.edu", Port => "1695");
+      my $line = $t->Net::Telnet::getline(Timeout => 4);
+      $callback->($line);
+      return 1;
+    }
+    return undef;
+}
+
+return "insult";
+
+__END__
+
+=head1 NAME
+
+insult.pm - Description
+
+insult module for flooterbuck infobot, using insulthost.colorado.edu:1695.
+
+=head1 PREREQUISITES
+
+	Net::Telnet
+
+=head1 DESCRIPTION
+
+Port of the Extras.pl code from stock infobot
+
+=head1 AUTHORS
+
+flooterbuck+insult.pm@richardharman.com
