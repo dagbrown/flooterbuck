ff4635726ae104da7530cc797a55e9390f2c607d
dagbrown <dagbrown>
2003-01-23 01:37:59 +0000

    Ran it through perltidy with all defaults.

--- modules/bash.pm
+++ modules/bash.pm
@@ -1,7 +1,6 @@
-# $Id: bash.pm,v 1.3 2003/01/22 21:37:06 rharman Exp $
+# $Id: bash.pm,v 1.4 2003/01/23 01:37:59 dagbrown Exp $
 package bash;
 use strict;
-
 
 =head1 NAME
 
@@ -35,7 +34,7 @@ Richard Harman <flooterbuck+bash.pm@richardharman.com>
 # Check that LWP is available, so we don't waste our time
 # generating error messages later on.
 #------------------------------------------------------------------------
-my ($no_bash, $no_posix);
+my ( $no_bash, $no_posix );
 
 BEGIN {
     eval qq{
@@ -56,11 +55,11 @@ BEGIN {
 # man's "lynx -dump".
 #------------------------------------------------------------------------
 sub strip_html($) {
-    my $blob=shift;
+    my $blob = shift;
     chomp $blob;
-    $blob=~s/\<[^>]+\>//g;
-    $blob=~s/\&[a-z]+\;?//g;
-    $blob=~s/\s+/ /g;
+    $blob =~ s/\<[^>]+\>//g;
+    $blob =~ s/\&[a-z]+\;?//g;
+    $blob =~ s/\s+/ /g;
     return $blob;
 }
 
@@ -71,22 +70,21 @@ sub strip_html($) {
 # and feed the auction ID number into the maw of auction_summary.
 #------------------------------------------------------------------------
 sub bash_getdata($) {
-    my $line=shift;
+    my $line = shift;
 
-    if($line =~ /bash\s+(\d+)/i && ($1 gt 0) ) {
-      return &get_quote($1); 
+    if ( $line =~ /bash\s+(\d+)/i && ( $1 gt 0 ) ) {
+        return &get_quote($1);
     }
 }
 
-sub bash::get_quote ($)
-{
-  my $quote_number = shift;
-  my $bash=LWP::Simple::get( 'http://bash.org/?' .$quote_number);
-  my ($quote) = ($bash =~ m/<p class="qt">(.+)<\/p>/sgi);
-  $quote =~ s/<br \/>//g;
-  $quote = HTML::Entities::decode($quote);
-  $quote =~ s/\r//g;
-  return split("\n",$quote); 
+sub bash::get_quote ($) {
+    my $quote_number = shift;
+    my $bash         = LWP::Simple::get( 'http://bash.org/?' . $quote_number );
+    my ($quote) = ( $bash =~ m/<p class="qt">(.+)<\/p>/sgi );
+    $quote =~ s/<br \/>//g;
+    $quote = HTML::Entities::decode($quote);
+    $quote =~ s/\r//g;
+    return split ( "\n", $quote );
 }
 
 #------------------------------------------------------------------------
@@ -95,27 +93,28 @@ sub bash::get_quote ($)
 # This handles the forking (or not) stuff.
 #------------------------------------------------------------------------
 sub bash::get($$) {
-    if($no_bash) {
+    if ($no_bash) {
         &main::status("Sorry, bash.pm requires LWP and couldn't find it");
         return "";
     }
 
-    my($line,$callback)=@_;
-    $SIG{CHLD}="IGNORE";
-    my $pid=eval { fork(); };         # Don't worry if OS isn't forking
+    my ( $line, $callback ) = @_;
+    $SIG{CHLD} = "IGNORE";
+    my $pid = eval { fork(); };    # Don't worry if OS isn't forking
     return 'NOREPLY' if $pid;
     my @lines = &bash_getdata($line);
-    if (! scalar @lines)
-    {
-      $callback->("Either that quote id does't exist, or bash.org is busted at the moment.");
-    } else {
-      foreach (@lines)
-      {
-	$callback->($_);
-	sleep 1;
-      }
+    if ( !scalar @lines ) {
+        $callback->(
+"Either that quote id does't exist, or bash.org is busted at the moment."
+        );
     }
-    if (defined($pid))                # child exits, non-forking OS returns
+    else {
+        foreach (@lines) {
+            $callback->($_);
+            sleep 1;
+        }
+    }
+    if ( defined($pid) )    # child exits, non-forking OS returns
     {
         exit 0 if ($no_posix);
         POSIX::_exit(0);
@@ -127,11 +126,11 @@ sub bash::get($$) {
 #------------------------------------------------------------------------
 
 sub scan(&$$) {
-    my ($callback, $message, $who)=@_;
+    my ( $callback, $message, $who ) = @_;
 
     if ( ::getparam('bash') and $message =~ /^\s*bash\s+(\d+)$/i ) {
         &main::status("bash playback");
-        &bash::get($message,$callback);
+        &bash::get( $message, $callback );
         return 1;
     }
 }
