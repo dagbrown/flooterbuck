b5bdd13a2aaee990727616597bc22617abdcbaa3
rich_lafferty <rich_lafferty>
2003-11-20 04:21:01 +0000

    'ping hostname' returns summary of ping results

---
+++ modules/ping.pm
@@ -0,0 +1,87 @@
+#------------------------------------------------------------------------
+# infobot module to report ping summary statistics.
+#------------------------------------------------------------------------
+
+package ping;
+
+my ($no_posix, $no_socket);
+
+BEGIN {
+    eval qq{
+        use Socket;
+    };
+    $no_socket++ if ($@);
+
+    eval qq{
+        use POSIX;
+    };
+    $no_posix++ if ($@);
+}
+
+sub ping {
+    my $host = shift;
+   
+    # look up host outside the shell, to avoid insertion attacks
+    my $inetaddr = gethostbyname($host);
+    return "I can't find $host in the DNS." unless $inetaddr;
+    my $addr = inet_ntoa(scalar gethostbyname($host));
+   
+    my $cmd = "/bin/ping -q -c 10 -w 15 $addr";
+    # http://www.amazon.com/exec/obidos/tg/detail/-/0140502416/
+    unless ( open (DUCK, "$cmd |") ) {
+        &main::status("$cmd returned $!");
+        return "/bin/ping returned an error."
+    }
+
+    my @out;
+    while (<DUCK>) {
+        chomp;
+        if (/^\d+ packets transmitted/ or /^rtt/) {
+            push (@out, $_);
+        }
+    }
+ 
+    close DUCK;
+    
+    if (@out) {
+        return join (", ", @out);
+    }
+    else {
+        # leave this to the admin to debug.
+        &main::status("$cmd returned something odd");
+        return "/bin/ping returned something I can't understand.";
+    }
+     
+}
+
+sub scan(&$$) {
+    my ($callback,$message,$who) = @_;
+    
+    if ($message=~/^\s*ping\s+(\S+)\D*$/) {
+
+        my $host = $1;
+        
+        if($no_socket) {
+            &main::status("Sorry, ping requires Socket.pm and can't find it");
+            return undef;
+        }
+        
+        $SIG{CHLD}="IGNORE";
+        my $pid=eval { fork(); };         # Don't worry if OS isn't forking
+        return "NOREPLY" if $pid;
+ 
+        my $response = ping($host);
+        
+        $callback->($response);
+        if (defined($pid))               # child exits, non-forking OS returns
+        {
+            exit 0 if ($no_posix);
+            POSIX::_exit(0);
+        }
+        return 1;
+    } else {
+        return undef;
+    }
+}
+
+"ping";
