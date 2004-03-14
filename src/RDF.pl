ecaa2bc4e3427c5d5451c1cfd934933c54760915
dagbrown <dagbrown>
2004-03-14 04:46:18 +0000

    Argh, indent.  Sorry.

--- src/RDF.pl
+++ src/RDF.pl
@@ -3,57 +3,61 @@
 # minor mods by lenzo@cs.cmu.edu
 
 BEGIN {
-  eval qq{
-    use LWP::UserAgent;
-    use XML::RSS;
-    use HTTP::Request::Common qw(GET);
-  };
+    eval qq{
+        use LWP::UserAgent;
+        use XML::RSS;
+        use HTTP::Request::Common qw(GET);
+    };
 
-  $no_headlines++ if($@);
+    $no_headlines++ if($@);
 }
 
 sub get_headlines {
-  my ($rdf_loc) = @_;
+    my ($rdf_loc) = @_;
 
-  if ($no_headlines) {
-    return "error: RDF headlines require LWP::UserAgent, XML::RSS, and HTTP::Request... sorry.";
-  }
-
-  if ($rdf_loc) {
-    &status("getting headlines from $rdf_loc");
-
-    my $ua = new LWP::UserAgent;
-    if (my $proxy = main::getparam('httpproxy')) { $ua->proxy('http', $proxy) };
-    $ua->timeout(10);
-
-    my $request = new HTTP::Request ("GET", $rdf_loc);
-    my $result = $ua->request ($request);
-
-    if ($result->is_success) {
-      my ($str);
-      $str = $result->content;
-      $rss = new XML::RSS;
-      eval { $rss->parse($str); };
-      if ($@) {
-	return "that gave some error";
-      } else {
-	my $return;
-
-	foreach my $item (@{$rss->{"items"}}) {
-	  $return .= $item->{"title"} . "; ";
-	  last if length($return) > $param{maxDataSize};
-	}
-
-	$return =~ s/; $//;
-
-	return $return;
-      }
-    } else {
-      return "error: $rdf_loc wasn't successful";
+    if ($no_headlines) {
+        return "error: RDF headlines require LWP::UserAgent, XML::RSS, and HTTP::Request... sorry.";
     }
-  } else {
-    return "error: no location stored for $where";
-  }
+
+    if ($rdf_loc) {
+        &status("getting headlines from $rdf_loc");
+
+        my $ua = new LWP::UserAgent;
+        if (my $proxy = main::getparam('httpproxy')) {
+            $ua->proxy('http', $proxy) 
+        };
+
+        $ua->timeout(10);
+
+        my $request = new HTTP::Request ("GET", $rdf_loc);
+        my $result = $ua->request ($request);
+
+        if ($result->is_success) {
+            my ($str, $error);
+            $str = $result->content;
+            $rss = new XML::RSS;
+            eval { $rss->parse($str); };
+            if ($@) {
+                return "that gave some error";
+                $error="$@";
+            } else {
+                my $return;
+
+                foreach my $item (@{$rss->{"items"}}) {
+                    $return .= $item->{"title"} . "; ";
+                    last if length($return) > $param{maxDataSize};
+                }
+
+                $return =~ s/; $//;
+
+                return $return;
+            }
+        } else {
+            return "error: $rdf_loc wasn't successful";
+        }
+    } else {
+        return "error: no location stored for $where";
+    }
 };
 
 1;
@@ -68,7 +72,7 @@ RDF.pl - Read RDF files into factoids
 
     LWP::UserAgent
     XML::RSS
-	HTTP::Request::Common
+        HTTP::Request::Common
 
 =head1 PARAMETERS
 
@@ -77,7 +81,7 @@ rss
 
 =head1 PUBLIC INTERFACE
 
-	<site> is <rss="site.rdf">
+        <site> is <rss="site.rdf">
 
 =head1 DESCRIPTION
 
